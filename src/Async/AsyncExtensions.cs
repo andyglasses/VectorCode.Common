f4b24302878361eaa10154c419d0009c6dee3528
Andy Glass <andyglasses@gmail.com>
2024-11-11 16:34:56 +0000

    added new mapped async list method

--- src/Async/AsyncExtensions.cs
+++ src/Async/AsyncExtensions.cs
@@ -22,4 +22,33 @@ public static class CollectionExtensions
     }
     return list;
   }
+
+  /// <summary>
+  /// Converts an IAsyncEnumerable to a List with a mapping function
+  /// </summary>
+  /// <typeparam name="T">List Type</typeparam>
+  /// <typeparam name="TOut">Mapped List Type</typeparam>
+  /// <param name="asyncEnumerable">Collection to parse</param>
+  /// <param name="mappingFunc"></param>
+  /// <param name="cancellationToken">Cancellation token</param>
+  /// <returns></returns>
+  public static async Task<List<TOut>> ToListAsync<T, TOut>(this IAsyncEnumerable<T> asyncEnumerable, Func<T, TOut> mappingFunc, CancellationToken? cancellationToken = null)
+  {
+    var list = new List<TOut>();
+    await foreach (var item in asyncEnumerable)
+    {
+      cancellationToken?.ThrowIfCancellationRequested();
+      if(item == null)
+      {
+        continue;
+      }
+      var mapped = mappingFunc(item);
+      if(mapped == null)
+      {
+        continue;
+      }
+      list.Add(mapped);      
+    }
+    return list;
+  }
 }
--- test/Async/AsyncExtensionTests.cs
+++ test/Async/AsyncExtensionTests.cs
@@ -10,7 +10,7 @@ public class AsyncExtensionsTests
 {
 
   [Test]
-  public async Task ToListAsync_Returns_Expected_Values()
+  public async Task ToListAsync_ShouldReturnTheList()
   {
     // Arrange
     var sourceList = new List<int> { 1, 2, 3 };
@@ -22,5 +22,53 @@ public class AsyncExtensionsTests
 
     // Assert
     actual.Should().BeEquivalentTo(sourceList);
+  }
+
+  [Test]
+  public async Task ToListAsync_WhenUsingMapping_ShouldReturnList()
+  {
+    // Arrange
+    var sourceList = new List<int> { 1, 2, 3 };
+    var expectedList = new List<string> { "m1", "m2", "m3" };
+    var pageCollection = Page<int>.FromValues(sourceList, null, new Mock<Response>().Object);
+    var asyncCollection = AsyncPageable<int>.FromPages(new[] { pageCollection });
+
+    // Act
+    var actual = await asyncCollection.ToListAsync(i => $"m{i}");
+
+    // Assert
+    actual.Should().BeEquivalentTo(expectedList);
+  }
+
+  [Test]
+  public async Task ToListAsync_WehnUsingMapping_ShouldIgnoreSourceNulls()
+  {
+    // Arrange
+    var sourceList = new List<string> { "1", "2", null!, "3" };
+    var expectedList = new List<string> { "m1", "m2", "m3" };
+    var pageCollection = Page<string>.FromValues(sourceList, null, new Mock<Response>().Object);
+    var asyncCollection = AsyncPageable<string>.FromPages(new[] { pageCollection });
+
+    // Act
+    var actual = await asyncCollection.ToListAsync(i => $"m{i}");
+
+    // Assert
+    actual.Should().BeEquivalentTo(expectedList);
+  }
+
+  [Test]
+  public async Task ToListAsync_WehnUsingMapping_ShouldIgnoreMappedNulls()
+  {
+    // Arrange
+    var sourceList = new List<int> { 1, 2, 3 };
+    var expectedList = new List<string> { "m1", "m3" };
+    var pageCollection = Page<int>.FromValues(sourceList, null, new Mock<Response>().Object);
+    var asyncCollection = AsyncPageable<int>.FromPages(new[] { pageCollection });
+
+    // Act
+    var actual = await asyncCollection.ToListAsync(i => i == 2 ? null! :  $"m{i}");
+
+    // Assert
+    actual.Should().BeEquivalentTo(expectedList);
   }
 }
