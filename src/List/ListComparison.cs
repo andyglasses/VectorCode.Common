11f5274773ce724de438fe1e8664f5e38a52a705
Andy Glass <andyglasses@gmail.com>
2024-11-18 14:03:35 +0000

    added list comparison using hashset

---
+++ src/List/ListComparison.cs
@@ -0,0 +1,28 @@
+﻿namespace VectorCode.Common.List;
+
+/// <summary>
+/// Utilities methods for comparing lists
+/// </summary>
+public static class ListComparison
+{
+  /// <summary>
+  /// Compares two lists and returns the items that are in both, deleted in the new list, and added in the new list
+  /// </summary>
+  /// <typeparam name="T">List type</typeparam>
+  /// <param name="origionalList">The origonal list</param>
+  /// <param name="newList">The new list</param>
+  /// <param name="toHashable">A method to create a hashable string</param>
+  /// <returns></returns>
+  public static (List<T> InBoth, List<T> DeletedInNew, List<T>AddedInNew) ComputeListDif<T>(this List<T> origionalList, List<T> newList, Func<T, string> toHashable)
+  {
+    var origionalHashSet = new HashSet<string>(origionalList.Select(toHashable));
+    var newHashSet = new HashSet<string>(newList.Select(toHashable));
+
+    var inBoth = origionalHashSet.Intersect(newHashSet).Select(hash => origionalList.First(item => toHashable(item) == hash)).ToList();
+    var deletedInNew = origionalHashSet.Except(newHashSet).Select(hash => origionalList.First(item => toHashable(item) == hash)).ToList();
+    var addedInNew = newHashSet.Except(origionalHashSet).Select(hash => newList.First(item => toHashable(item) == hash)).ToList();
+
+    return (inBoth, deletedInNew, addedInNew);
+
+  }
+}
---
+++ test/ListComparisonTests.cs
@@ -0,0 +1,22 @@
+﻿using VectorCode.Common.List;
+
+namespace VectorCode.Common.Test;
+
+[TestFixture]
+public class ListComparisonTests
+{
+  [Test]
+  public void ComputeListDif_WhenCalled_ReturnsInBothDeletedInNewAndAddedInNew()
+  {
+    // Arrange
+    var origionalList = new List<string> { "a", "b", "c" };
+    var newList = new List<string> { "b", "c", "d" };
+    Func<string, string> toHashable = (item) => item;
+    // Act
+    var result = ListComparison.ComputeListDif(origionalList, newList, toHashable);
+    // Assert
+    Assert.That(result.InBoth, Is.EquivalentTo(new List<string> { "b", "c" }));
+    Assert.That(result.DeletedInNew, Is.EquivalentTo(new List<string> { "a" }));
+    Assert.That(result.AddedInNew, Is.EquivalentTo(new List<string> { "d" }));
+  }
+}
